8990baf43edf597cac1fe179b24b50e00587f5af
SMR <SmugglerSMR@gmail.com>
2017-05-28 21:24:34 +1000

    Possible fix for light. Code at presentable state

--- Debug/serial.d
+++ Debug/serial.d
@@ -1,22 +1,22 @@
 serial.d serial.o: .././serial.c \
- d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
- d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
- d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
- d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
- d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
- d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
  .././lib/usb_serial/usb_serial.h
 
-d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
 
-d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
 
-d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
 
-d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
 
-d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
 
-d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
 
 .././lib/usb_serial/usb_serial.h:
--- main.c
+++ main.c
@@ -2,7 +2,7 @@
  * main.c - EGB240 Digital Voice Recorder Skeleton Code
  *
  * This code provides a skeleton implementation of a digital voice 
- * recorder using the Teensy microcontroller and QUT TensyBOBv2 
+ * recorder using the Teensy micro controller and QUT TensyBOBv2 
  * development boards. This skeleton code demonstrates usage of
  * the EGB240DVR library, which provides functions for recording
  * audio samples from the ADC, storing samples temporarily in a 
@@ -24,16 +24,17 @@
  * A serial USB interface is provided as a secondary control and
  * debugging interface. Errors will be printed to this interface.
  *
- * Version: v1.2
- *    Date: 28/05/2017
- *  Author: Mark Broadmeadow
- *  E-mail: mark.broadmeadow@qut.edu.au
- *	Edited: Group 420
+ * Version:				v1.3
+ *    Date:				28/05/2017
+ *  Author:				Mark Broadmeadow
+ *  E-mail:				mark.broadmeadow@qut.edu.au
+ *	Edited:				Group 420
+ *	GitHub Repository:	https://github.com/SmugglerSMR/EGB240_Recorder/
  */  
 
- /************************************************************************/
- /* INCLUDED LIBRARIES/HEADER FILES                                      */
- /************************************************************************/
+/************************************************************************/
+/* INCLUDED LIBRARIES/HEADER FILES                                      */
+/************************************************************************/
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
@@ -51,9 +52,11 @@
 #define SET_BIT(byte, bit) (byte |= (1 << bit))
 
 //Macros for if conditions//
-#define BIT_IS_SET(byte, bit) (byte & (1 << bit))           // check to see if the bit is set or not
+#define BIT_IS_SET(byte, bit) (byte & (1 << bit))  // check to see if 
+												   //     the bit is set or not
 
-#define TOP 255
+#define TOP 255									   // Init 0xFF 
+#define pageSize 512							   // Init Size of the Page
 
 /************************************************************************/
 /* ENUM DEFINITIONS                                                     */
@@ -68,16 +71,18 @@ enum {
 /* GLOBAL VARIABLES                                                     */
 /************************************************************************/
 volatile uint16_t pageCount = 0;	// Page counter - used to terminate recording
-volatile uint16_t newPage = 0;		// Flag that indicates a new page is available for read/write
-volatile uint8_t stop = 0;			// Flag that indicates playback/recording is complete
+volatile uint16_t newPage = 0;		// Flag that indicates a new page 
+									//					is available for read/write
+volatile uint8_t stop = 0;			// Flag that indicates playback/recording
+									//						 is complete
 
-// Personal global variables
-volatile uint32_t data_amount = 0;
-volatile uint8_t played = 1;
-volatile uint8_t first_que = 0;
-volatile uint8_t second_que = 0;
-volatile uint8_t first_played = 0;
-volatile uint8_t second_played = 0;
+// Personal global variables for playback
+volatile uint32_t data_amount = 0;	// Amount of samples used to play
+volatile uint8_t played = 1;		// Flag used in interrupt for Ini
+volatile uint8_t first_que = 0;		// First sample
+volatile uint8_t second_que = 0;	// Next sample
+volatile uint8_t first_played = 0;	// Flag indicates if first sample was played
+volatile uint8_t second_played = 0;	// Flag indicates if second sample was played
 /************************************************************************/
 /* FUNCTION PROTOTYPES                                                  */
 /************************************************************************/
@@ -90,55 +95,57 @@ void pageEmpty();
 
 // Initialize PLL (required by USB serial interface, PWM)
 void pll_init() {
-	PLLFRQ = 0x6A; // PLL = 96 MHz, USB = 48 MHz, TIM4 = 64 MHz
+	PLLFRQ = 0x6A;				// PLL = 96 MHz, USB = 48 MHz, TIM4 = 64 MHz
 }
 
 // Configure system clock for 16 MHz
 void clock_init() {
-	CLKPR = 0x80;	// Prescaler change enable
-	CLKPR = 0x00;	// Prescaler /1, 16 MHz
+	CLKPR = 0x80;				// Prescaler change enable
+	CLKPR = 0x00;				// Prescaler /1, 16 MHz
 }
 
 // Initialize buttons and LEDs
 void hardware_setup (){
 	// clear the bit and set the button pins as input (1= output && 0=input)		
-	DDRD |= 0b11110000; // LEDS
-	DDRF &= ~0b11110000; // Buttons
+	DDRD |= 0b11110000;			// LEDS
+	DDRF &= ~0b11110000;		// Buttons
 }
 
+// Initialize PWM state. Sets Prescaler to 8
 void set_pwm() {	
-	OCR4C = TOP;			// set top to 0xFF (255)
-	
-	TCCR4B = 0x04;				//Prescaler 8, ~32.5kHz	
-	TCCR4A = 0x20;				//set control register for 0C4B
-	OCR4B = 0x80;				// initialize to 50% duty cycle
+	OCR4C = TOP;				// Set top to 0xFF (255)	
+	TCCR4B = 0x04;				// Prescaler 8, ~32.5kHz	
+	TCCR4A = 0x20;				// Set control register for 0C4B Off
+	OCR4B = 0x80;				// Initialize to 50% duty cycle
 	TIMSK4 = 0x00;				// Disable interrupt	
-	DDRB |= 0b01000000;			// set pin B6 to output(JOUT)
-	TCNT4 = 0x00;  // reset counter
+	DDRB |= 0b01000000;			// Set pin B6 to output(JOUT)
+	TCNT4 = 0x00;				// Reset counter
 }
 void start_pwm(){
 	TIMSK4 = 0x04;				// Enable interrupt
-	TCCR4A = 0x21;				//set control register for 0C4B
+	TCCR4A = 0x21;				// Set control register for 0C4B On
 }
 void stop_pwm(){
 	TIMSK4 = 0x00;				// Disable interrupt
-	TCCR4A = 0x20;				//set control register for 0C4B
+	TCCR4A = 0x20;				// Set control register for 0C4B Off
 }
 // Initialize DVR subsystems and enable interrupts
 void init() {
-	cli();			// Disable interrupts
-	clock_init();	// Configure clocks
-	pll_init();     // Configure PLL (used by Timer4 and USB serial)
-	serial_init();	// Initialize USB serial interface (debug)
-	timer_init();	// Initialize timer (used by FatFs library)
-	hardware_setup();  // Initialize Button with LEDs
+	cli();						// Disable interrupts
+	clock_init();				// Configure clocks
+	pll_init();					// Configure PLL (used by Timer4 and USB serial)
+	serial_init();				// Initialize USB serial interface (debug)
+	timer_init();				// Initialize timer (used by FatFs library)
+	hardware_setup();			// Initialize Button with LEDs
 	set_pwm();
-	buffer_init(pageFull, pageEmpty);  // Initialize circular buffer (must specify callback functions)
-	adc_init();		// Initialize ADC
-	sei();			// Enable interrupts
+	buffer_init(pageFull,
+				   pageEmpty);  // Initialize circular buffer 
+								//				(must specify callback functions)
+	adc_init();					// Initialize ADC
+	sei();						// Enable interrupts
 	
 	// Must be called after interrupts are enabled
-	wave_init();	// Initialize WAVE file interface
+	wave_init();				// Initialize WAVE file interface
 }
 /************************************************************************/
 /* CALLBACK FUNCTIONS FOR CIRCULAR BUFFER                               */
@@ -148,16 +155,16 @@ void init() {
 void pageFull() {
 	if(!(--pageCount)) {
 		// If all pages have been read
-		adc_stop();		// Stop recording (disable new ADC conversions)
-		stop = 1;		// Flag recording complete
+		adc_stop();				// Stop recording (disable new ADC conversions)
+		stop = 1;				// Flag recording complete
 	} else {
-		newPage = 1;	// Flag new page is ready to write to SD card
+		newPage = 1;			// Flag new page is ready to write to SD card
 	}
 }
 
 // CALLED FROM BUFFER MODULE WHEN A NEW PAGE HAS BEEN EMPTIED
 void pageEmpty() {
-	if (data_amount > 2048) {
+	if (data_amount > (4*pageSize)) {	// If Data reached final 2 page
 		newPage = 1;
 	}	
 }
@@ -168,97 +175,98 @@ void pageEmpty() {
 
 // Initiates a record cycle
 void dvr_record() {
-	buffer_reset();		// Reset buffer state
+	buffer_reset();				// Reset buffer state
 	
-	pageCount = 305;	// Maximum record time of 10 sec
-	newPage = 0;		// Clear new page flag
+	pageCount = 305;			// Maximum record time of 10 sec
+	newPage = 0;				// Clear new page flag
 	
-	wave_create();		// Create new wave file on the SD card
-	adc_start();		// Begin sampling
+	wave_create();				// Create new wave file on the SD card
+	adc_start();				// Begin sampling
 
-	SET_BIT (PORTD, PD1); // turn on the first led
-	PORTD &= 0b00001111;  // turn other LEDs off
+	SET_BIT (PORTD, PD1);		// turn on the first led
+	PORTD &= 0b00001111;		// turn other LEDs off
 }
 
 /************************************************************************/
 /* MAIN LOOP (CODE ENTRY)                                               */
 /************************************************************************/
 int main(void) {
-	uint8_t state = DVR_STOPPED;	// Start DVR in stopped state	
+	uint8_t state = DVR_STOPPED;// Start DVR in stopped state	
 	// Initialization
 	init();	
-	PORTD &= 0b00001111;  // turn other LEDs off
+	PORTD &= 0b00001111;		// turn other LEDs off
 	// Loop forever (state machine)
 	stop_pwm();
     for(;;) {		
 		// Switch depending on state
 		switch (state) {
 			case DVR_STOPPED:				
-				if ( BIT_IS_SET (~PINF, PF5 ) ) {
-				//if(~PINF & (1 << 5 ))	  {
-					PORTD |= 0b10000000;  // turn LED2 on				
+				if ( BIT_IS_SET (~PINF, PF5 ) ) {			// -----STARTING THE RECORDING----
+					PORTD |= 0b10000000;					// Turn LED2 on				
 					
-					printf("Recording started...");	// Output status to console
-					dvr_record();			// Initiate recording
-					state = DVR_RECORDING;	// Transition to "recording" state
-				 }
-				 if ( BIT_IS_SET (~PINF, PF4 ) ) {
-				 //if(~PINF & (1 << 4 ))	  {
-					 PORTD &= 0b00001111;  // turn all LEDs off
-					 PORTD |= 0b01000000;  // turn LED3 on
+					printf("Recording started...");			// Output status to console
+					dvr_record();							// Initiate recording
+					state = DVR_RECORDING;					// Transition to "recording" state
+				 }											// -------------------------------
+				 if ( BIT_IS_SET (~PINF, PF4 ) ) {			// -------STARTING PLAYBACK-------
+				 	 PORTD &= 0b00001111;					// Turn all LEDs off
+					 PORTD |= 0b01000000;					// turn LED3 on
 					 
-					 printf("Preparing file\n");	// Output status to console
+					 printf("Preparing file\n");			// Output status to console
 					 buffer_reset();
 					 newPage = 0;
-					 data_amount = wave_open ()*2+1;  //Open the file to read not VOID function
+					 data_amount = wave_open ()*2+1;		// Open the file to read not VOID function
 					 
-					 wave_read (buffer_writePage(), 512); //2 is the number of counts
-					 wave_read (buffer_writePage(), 512); //2 is the number of counts
-					 start_pwm();					 
-					 state = DVR_PLAYING;	// Transition to "recording" state
-				 }
+					 wave_read (buffer_writePage(),
+											   pageSize);   // Feel first page with samples
+					 wave_read (buffer_writePage(),
+											   pageSize);   // Feels second page with samples
+					 start_pwm();							// Start PWM					 
+					 state = DVR_PLAYING;					// Transition to "Playing" state
+				 }											// ----------------------------------
 				break;
-			case DVR_RECORDING:				
-				if ( BIT_IS_SET (~PINF, PF6) ) {
-					PORTD &= 0b00001111;  // turn all LEDs off
-					PORTD |= 0b00010000;  // turn LED1 on					
-					pageCount = 1;	// Finish recording last page									
-				}
+			case DVR_RECORDING:
+				PORTD |= 0b10000000;						// Keeps LED2 turn on
+				if ( BIT_IS_SET (~PINF, PF6) ) {			// --- STOP REcording on Button Press--
+					PORTD &= 0b00001111;					// Turn all LEDs off
+					PORTD |= 0b00010000;					// Turn LED1 on					
+					pageCount = 1;							// Finish recording last page									
+				}											// ----------------------------------
 			
-				// Write samples to SD card when buffer page is full
-				if (newPage) {
-					newPage = 0;	// Acknowledge new page flag
-					wave_write(buffer_readPage(), 512);
-				} else if (stop) {
-					// Stop is flagged when the last page has been recorded
-					stop = 0;							// Acknowledge stop flag
-					wave_write(buffer_readPage(), 512);	// Write final page
-					wave_close();						// Finalize WAVE file 
-					printf("Recording COMPLETE!\n");	// Print status to console
-					state = DVR_STOPPED;				// Transition to stopped state
-				}
+				if (newPage) {								// ---Write samples to SD card when buffer page is full---
+					newPage = 0;							// Acknowledge new page flag
+					wave_write(buffer_readPage(), pageSize);
+				} else if (stop) {							// ---Stop is flagged when the last page has been recorded---
+					stop = 0;								// Acknowledge stop flag
+					wave_write(buffer_readPage(),
+												 pageSize);	// Write final page
+					wave_close();							// Finalize WAVE file 
+					printf("Recording COMPLETE!\n");		// Print status to console
+					state = DVR_STOPPED;					// Transition to stopped state
+				}											// --------------------------------------------------------
 				break;
 			case DVR_PLAYING:
-				PORTB |= 0b01000000;
+				PORTB |= 0b01000000;						// Keeps LED3 turn on
 				
-				if ( BIT_IS_SET (~PINF, PF6) ) {
-					PORTD &= 0b00001111;  // turn other LEDs off
-					PORTD |= 0b00010000;  // turn LED1 on
+				if ( BIT_IS_SET (~PINF, PF6) ) {			// ---- Stops PLayback------
+					PORTD &= 0b00001111;					// turn other LEDs off
+					PORTD |= 0b00010000;					// turn LED1 on
 					
-					stop = 1;
-					newPage = 0;
-					stop_pwm();
-				}								
-				if(newPage){
+					stop = 1;								// Sets stop flag
+					newPage = 0;							// Finalize page
+					stop_pwm();								// Stops PWM
+				}											// --------------------------				
+				if(newPage){								// ------Page is reeded
 					newPage = 0;					
-					wave_read (buffer_writePage(), 512); //2 is the number of counts
-				}
-				else if(stop) {
+					wave_read (buffer_writePage(), 
+												pageSize);  // Writes next page
+				}											//---------------------------
+				else if(stop) {								//---- Finalize Playback------
 					stop = 0;					
-					wave_close (); // close the file after reading
-					printf("DONE!");
-					state = DVR_STOPPED;				// Transition to stopped state
-				}
+					wave_close ();							// close the file after reading
+					printf("DONE!");						
+					state = DVR_STOPPED;					// Transition to stopped state
+				}											//-----------------------------
 				
 				break;
 			default:
@@ -276,32 +284,32 @@ int main(void) {
 /**
  * ISR: PWM conversion complete
  * 
+ * Creates an average value to fill space. (var1+var2)/2 RUns per 3 sample
  */
 ISR(TIMER4_OVF_vect) {
-	// 	
-	if(--data_amount > 0){		
-		if(played){
-			first_que = buffer_dequeue();
-			second_que = buffer_dequeue();				
-			first_played = 0;
-			second_played = 0;
-			played = 0;
-		} else {
-			if(!first_played) {
+	if(--data_amount > 0){								// -----Runs until all samples were played
+		if(played){										// True if 2 samples has not been played yet.		
+			first_que = buffer_dequeue();				// Stores first value
+			second_que = buffer_dequeue();				// Stores second value
+			first_played = 0;							// Sets flag for first
+			second_played = 0;							// Sets flag for second
+			played = 0;									// Values are stored, start play
+		} else {										// ------Plays samples of creates one--------
+			if(!first_played) {							// ------Play first sample-------------------
 				OCR4B = first_que;
 				first_played = 1;
-			} else if(!second_played) {
+			} else if(!second_played) {					// ------Play average sample-----------------
 				OCR4B = (first_que+second_que)/2.0;
 				second_played = 1;
-			}
-			else {
+			} else {									// ------Play second sample-------------------
 				OCR4B = second_que;				
 				played = 1;
-			}
-		}	
-	} else {
-		newPage = 0;
-		stop = 1;
-		stop_pwm();			
-	}
-}
+			}											// --------------------------------------------
+		}												// --------------------------------------------
+	} else {											// ----- File has been played------------------
+		newPage = 0;									// Empties the page
+		stop = 1;										// Stops playback run
+		stop_pwm();										// Stops PWM
+	} // END data_amount								// --------------------------------------------
+	
+} // END Interrupt
